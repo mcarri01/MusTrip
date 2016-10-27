a7e48d7f8d7a7167e2f58c1438df8cd44e382d30
mcarri01 <matthew.carrington_fair@tufts.edu>
2016-10-26 20:00:34 -0400

    added user tests and fixed asserts to check result

--- flask_files/test.py
+++ flask_files/test.py
@@ -1,8 +1,14 @@
 import os
+import json
 import mustrip
 import unittest
 import tempfile
+import string
+import random
 from random import randint
+
+def generate_user():
+        return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
 class mustripTestCase(unittest.TestCase):
 
@@ -17,19 +23,34 @@ class mustripTestCase(unittest.TestCase):
 
     def test_coords(self):
         for i in range(50):
-            req = self.app.post('/getPlaylist', data=dict(
+            res = self.app.post('/getPlaylist', data=dict(
                 lat=str(randint(0, 100)),
                 lng=str(randint(0,100))
             ))
-            self.assertEqual(req.status, '200 OK')
+            self.assertEqual(res.status, '200 OK')
+
     def test_city(self):
-        req = self.app.post('/playlistbycity', data=dict(
+        res = self.app.post('/playlistbycity', data=dict(
             city="Boston",
         ))
-        self.assertEqual(req.status, '200 OK')
-        req = self.app.post('/playlistbycity', data=dict(
-            city="Washington D.C."
+        data = json.loads(res.get_data(as_text=True))
+        self.assertEqual(data['city'], "Boston")
+
+
+    def test_existing_user(self):
+        res = self.app.post('/addUser', data=dict(
+            user="Test",
         ))
+        data = json.loads(res.get_data(as_text=True))
+        self.assertEqual(data['status'], "exists")
+
+    def test_new_user(self):
+        user = generate_user()
+        res = self.app.post("/addUser", data=dict(
+            user= user,
+        ))
+        data = json.loads(res.get_data(as_text=True))
+        self.assertEqual(data['status'], "success")
 
 if __name__ == '__main__':
     unittest.main()
